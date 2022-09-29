79a891543b6ca572f4ce9de300b3a5f3bcc2f65b
NickJohnson868 <624536826@qq.com>
2022-09-29 11:48:56 +0800

    项目

---
+++ Inject/Inject.cpp
@@ -0,0 +1,200 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <atlstr.h> //CString的头文件
+#include <Tlhelp32.h>
+
+LPCTSTR exeName = _T("WeChat.exe");
+LPCTSTR dllName = _T("hookDll.dll");
+const char * DLLPath = "C:\\Users\\62453\\Documents\\projects\\hookDll\\Release\\hookDll.dll";
+
+// 获取PID
+UINT GetProcessIdByName(LPCTSTR pszExeFile)
+{
+	UINT nProcessID = 0;
+	PROCESSENTRY32 pe = { sizeof(PROCESSENTRY32) };
+
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+
+	if (hSnapshot != INVALID_HANDLE_VALUE)
+	{
+		if (Process32First(hSnapshot, &pe))
+		{
+			while (Process32Next(hSnapshot, &pe))
+			{
+				if (lstrcmpi(pszExeFile, pe.szExeFile) == 0)
+				{
+					nProcessID = pe.th32ProcessID;
+					break;
+				}
+			}
+		}
+		CloseHandle(hSnapshot);
+	}
+	return nProcessID;
+}
+
+// 提升进程访问权限
+bool enableDebugPriv()
+{
+	HANDLE hToken;
+	LUID sedebugnameValue;
+	TOKEN_PRIVILEGES tkp;
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)
+		)
+	{
+		return false;
+	}
+	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &sedebugnameValue))
+	{
+		CloseHandle(hToken);
+		return false;
+	}
+	tkp.PrivilegeCount = 1;
+	tkp.Privileges[0].Luid = sedebugnameValue;
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tkp, sizeof(tkp), NULL, NULL))
+	{
+		CloseHandle(hToken);
+		return false;
+	}
+	return true;
+}
+
+// 卸载DLL
+bool UnInjectDll(const TCHAR* ptszDllFile, DWORD dwProcessId)
+{
+	if (dwProcessId == 0)
+	{
+		MessageBox(NULL,
+			L"The target process have not been found !",
+			L"Notice",
+			MB_ICONINFORMATION | MB_OK
+		);
+		return FALSE;
+	}
+
+	// 参数无效   
+	if (NULL == ptszDllFile || 0 == ::_tcslen(ptszDllFile))
+	{
+		return false;
+	}
+	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+	HANDLE hProcess = NULL;
+	HANDLE hThread = NULL;
+	// 获取模块快照   
+	hModuleSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
+	if (INVALID_HANDLE_VALUE == hModuleSnap)
+	{
+		return false;
+	}
+	MODULEENTRY32 me32;
+	memset(&me32, 0, sizeof(MODULEENTRY32));
+	me32.dwSize = sizeof(MODULEENTRY32);
+	// 开始遍历   
+	if (FALSE == ::Module32First(hModuleSnap, &me32))
+	{
+		::CloseHandle(hModuleSnap);
+		return false;
+	}
+	// 遍历查找指定模块   
+	bool isFound = false;
+	do
+	{
+		isFound = (0 == ::_tcsicmp(me32.szModule, ptszDllFile) || 0 == ::_tcsicmp(me32.szExePath, ptszDllFile));
+		if (isFound) // 找到指定模块   
+		{
+			break;
+		}
+	} while (TRUE == ::Module32Next(hModuleSnap, &me32));
+	::CloseHandle(hModuleSnap);
+	if (false == isFound)
+	{
+		return false;
+	}
+	// 获取目标进程句柄   
+	hProcess = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
+	if (NULL == hProcess)
+	{
+		return false;
+	}
+	// 从 Kernel32.dll 中获取 FreeLibrary 函数地址   
+	LPTHREAD_START_ROUTINE lpThreadFun = (PTHREAD_START_ROUTINE)::GetProcAddress(::GetModuleHandle(_T("Kernel32")), "FreeLibrary");
+	if (NULL == lpThreadFun)
+	{
+		::CloseHandle(hProcess);
+		return false;
+	}
+	// 创建远程线程调用 FreeLibrary   
+	hThread = ::CreateRemoteThread(hProcess, NULL, 0, lpThreadFun, me32.modBaseAddr /* 模块地址 */, 0, NULL);
+	if (NULL == hThread)
+	{
+		::CloseHandle(hProcess);
+		return false;
+	}
+	// 等待远程线程结束   
+	::WaitForSingleObject(hThread, INFINITE);
+	// 清理   
+	::CloseHandle(hThread);
+	::CloseHandle(hProcess);
+	std::cout << "卸载成功" << std::endl;
+	return true;
+}
+
+// 注入DLL
+bool InjectDll(DWORD pid)
+{
+	HANDLE hprocess = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+	if (!hprocess)
+	{
+		std::cout << "can not get handle" << std::endl;
+		return 1;
+	}
+	SIZE_T PathSize = (strlen(DLLPath) + 1) * sizeof(TCHAR);
+	LPVOID StartAddress = VirtualAllocEx(hprocess, NULL, PathSize, MEM_COMMIT, PAGE_READWRITE);
+	if (!StartAddress)
+	{
+		std::cout << "开辟内存失败" << std::endl;
+		return 1;
+	}
+	if (!WriteProcessMemory(hprocess, StartAddress, DLLPath, PathSize, NULL))
+	{
+		std::cout << "无法写入DLL路径" << std::endl;
+		return 1;
+	}
+	PTHREAD_START_ROUTINE pfnStartAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+	if (!pfnStartAddress)
+	{
+		std::cout << "无法获取函数地址" << std::endl;
+		return 1;
+	}
+	HANDLE hThread = CreateRemoteThreadEx(hprocess, NULL, NULL, pfnStartAddress, StartAddress, NULL, NULL, NULL);
+	if (!hThread)
+	{
+		std::cout << "创建线程失败" << std::endl;
+		return 1;
+	}
+	//WaitForSingleObject(hThread, INFINITE);//等待DLL结束
+	std::cout << "注入成功" << std::endl;
+	CloseHandle(hThread);
+	CloseHandle(hprocess);
+}
+
+int main()
+{
+	// 是否注入dll
+	bool in = true;
+
+	// 提高访问权限
+	enableDebugPriv();
+	// 获取微信的进程ID
+	DWORD pid = GetProcessIdByName(exeName);
+	// 卸载DLL
+	UnInjectDll(dllName, pid);
+	if (in)
+	{
+		// 注入
+		InjectDll(pid);
+	}
+
+	return 0;
+}
---
+++ hookDll/dllmain.cpp
@@ -0,0 +1,166 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <atlstr.h> //CString的头文件
+
+HMODULE GetWechatWin()
+{
+	return GetModuleHandle(L"WeChatWin.dll");
+}
+
+struct wxMsg
+{
+	wchar_t* pStr;
+	int strLen;
+	int strLen2;
+	char buff[0x34];
+};
+struct wxId
+{
+	wchar_t* pStr;
+	int strLen;
+	int strLen2;
+	char buff[0x8];
+};
+struct wxAt {
+	wxId* atid;
+	DWORD p1;
+	DWORD p2;
+	int end = 0;
+};
+
+void test()
+{
+	HMODULE hModule = GetWechatWin();
+	DWORD WXIDOFFSET = 0x25357A0;
+	DWORD PHONEOFFSET = 0x2535B70;
+	DWORD pwxid = (DWORD)hModule + WXIDOFFSET;
+	DWORD pphone = (DWORD)hModule + PHONEOFFSET;
+
+	// char* wxid = (char*)pwxid;
+	// char* phone = (char*)pphone;
+
+	CString s;
+	s.Format(_T("%x"), hModule);
+	MessageBox(NULL, s.GetBuffer(), L"WeChatWin.dll", MB_OK);
+}
+
+void SendWechatMessage(wchar_t* wxid, wchar_t* msg)
+{
+	__asm {
+		push 0x1;
+		mov eax, 0;
+		push eax;
+	}
+	
+	wxMsg text = { 0 };
+	text.pStr = msg;
+	text.strLen = wcslen(msg);
+	text.strLen2 = wcslen(msg) * 2;
+	char* pWxmsg = (char*)&text.pStr;
+
+	__asm {
+		push 1;
+		mov eax, 0;
+		push eax;
+		mov edi, pWxmsg;
+		push edi;
+	}
+
+	wxMsg id = { 0 };
+	id.pStr = wxid;
+	id.strLen = wcslen(wxid);
+	id.strLen2 = wcslen(wxid) * 2;
+	char* pWxid = (char*)&id.pStr;
+
+	char buff[0x81C] = { 0 };
+
+	__asm {
+		push 1;
+		mov eax, 0;
+		push eax;
+		mov edi, pWxmsg;
+		push edi;
+		mov edx, pWxid;
+		lea ecx, buff;
+	}
+
+	DWORD dwSendCallAddr = (DWORD)GetWechatWin() + (0x6658d2e0 - 0x65fc0000);
+	__asm {
+		push 1;
+		mov eax, 0;
+		push eax;
+		mov edi, pWxmsg;
+		push edi;
+		mov edx, pWxid;
+		lea ecx, buff;
+		call dwSendCallAddr;
+		add esp, 0xC;
+	}
+}
+
+void SendWechatMessage(wchar_t* wxid, wchar_t* msg, wchar_t* atsb)
+{
+	//拿到发送消息的call的地址
+	DWORD dwSendCallAddr = (DWORD)GetWechatWin() + (0x6658d2e0 - 0x65fc0000);
+
+	//微信ID/群ID
+	wxMsg id = { 0 };
+	id.pStr = wxid;
+	id.strLen = wcslen(wxid);
+	id.strLen2 = wcslen(wxid) * 2;
+
+	//消息内容
+	wxMsg text = { 0 };
+	text.pStr = msg;
+	text.strLen = wcslen(msg);
+	text.strLen2 = wcslen(msg) * 2;
+
+	wxAt at = { 0 };
+	if (atsb != NULL) {
+
+		wxId atid = { 0 };
+		atid.pStr = atsb;
+		atid.strLen = wcslen(atsb);
+		atid.strLen2 = wcslen(atsb) * 2;
+		at.atid = (wxId*)&atid.pStr;
+		at.p1 = (DWORD)(at.atid + sizeof(atsb) / 4);
+		at.p2 = at.p1;
+	}
+	//取出微信ID和消息的地址
+	char* pWxid = (char*)&id.pStr;
+	char* pWxmsg = (char*)&text.pStr;
+
+	char buff[0x81C] = { 0 };
+	//调用微信发送消息call
+	__asm {
+		push 1;
+		lea eax, at;
+		push eax;
+		mov edi, pWxmsg;
+		push edi;
+		mov edx, pWxid;
+		lea ecx, buff;
+		call dwSendCallAddr;
+		add esp, 0xC;
+	}
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		/*我们的代码，注入DLL后会执行*/
+		// MessageBox(0, L"成功潜入微信内部！", L"报告主人", NULL);
+		test();
+		SendWechatMessage((wchar_t*)L"wxid_q2mirynz7h6622", (wchar_t*)L"DLL成功注入，并向你发送了一条消息。");
+		// SendWechatMessage((wchar_t*)L"22526049857@chatroom", (wchar_t*)L"@王子豪 DLL成功注入，并向你发送了一条消息。", (wchar_t*)L"wxid_ivxugfki4phi22");
+	case DLL_THREAD_ATTACH:
+
+	case DLL_THREAD_DETACH:
+
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
